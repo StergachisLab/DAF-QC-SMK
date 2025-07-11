49093542480da1559a0cc9febdf85a5f8a612121
Stephanie Bohaczuk <stephanie.bohaczuk@gmail.com>
2025-07-11 14:36:50 -0700

    improved sequence_metrics.py speed

--- workflow/scripts/sequence_metrics.py
+++ workflow/scripts/sequence_metrics.py
@@ -2,7 +2,7 @@ import pysam
 import numpy as np
 import pandas as pd
 import os
-
+#import time
 
 # Inputs: aligned bam file with MD tags, regions (string), output file location
 # outputs: csv or text files of data for each region, list of CT and GA reads, possibly: bam file with ambiguity codes
@@ -21,8 +21,8 @@ summary_metrics = snakemake.output.summary_metrics
 #targeting_metrics = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/results/htt_test/qc/reads/htt_test.detailed_targeting_metrics.tbl.gz"
 #chimera_cutoff = 0.9
 #min_deamination_count = 50 # minimum number of deaminations to designate a strand (otherwise undetermined)
-#read_metrics = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/results/htt_test/qc/reads/htt_test_manual.read_metrics.tsv.gz"
-#summary_metrics = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/results/htt_test/qc/reads/htt_test_manual.summary_metrics.tsv.gz"
+#read_metrics = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/results/htt_test/qc/reads/htt_test_manual.read_metrics_optimized.tsv.gz"
+#summary_metrics = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/results/htt_test/qc/reads/htt_test_manual.summary_metrics_optimized.tsv.gz"
 
 
 
@@ -33,117 +33,96 @@ def parse_region(region):
     return chrom, start, end
 
 
-def determine_da_strand(read, cutoff=0.9): # Modified from Elliott's script
-    # based on the proportion of C->T & G->A determine the strand acted upon by DddA
+
+def strand_metrics(read, cutoff=0.9):
+        # based on the proportion of C->T & G->A determine the strand acted upon by DddA
     # only counting single base substitutions
 
     c = 0
     g = 0
-    total = 0
+    non_ref = 0
+
+#    start_strand = time.time()
 
     seq = read.query_sequence
     pair = read.get_aligned_pairs(matches_only=False, with_seq=True)
 
+
     for pos in pair:
-        if pos[0] == None or pos[1] == None: # indel, ignore
-            pass
+        if pos[0] is None or pos[1] is None: # indel, ignore
+            continue
         else:
             strand_base= seq[pos[0]]
 #            qi = pos[0]
             ref_base = pos[2].upper()
             if strand_base != ref_base:
-                total += 1
-                change = ref_base + strand_base
-                if change == "CT":
+                non_ref += 1
+                if ref_base == 'C' and strand_base == 'T':
                     c += 1
-                elif change == 'GA':
+                elif ref_base == 'G' and strand_base == 'A':
                     g += 1
 
-    if c+g == 0:
-        return('none')
-    elif c+g/total <= cutoff or c+g < min_deamination_count:
-        return('undetermined')
-    elif c/(c+g) >= cutoff:
-        return('CT')
-    elif g/(c+g) >= cutoff:
-        return('GA')
-    else:
-        return('chimera')
     
+    if c+g == 0:
+        strand='none'
+    elif c+g/non_ref <= cutoff or c+g < min_deamination_count:
+        strand = 'undetermined'
+    elif c/(c+g) >= cutoff:
+        strand = 'CT'
+    elif g/(c+g) >= cutoff:
+        strand = 'GA'
+    else:
+        strand = 'chimera'
+    
+#    print("designate_strand", time.time()-start_strand)
 
-def strand_metrics(read, strand):
-    # Requires CT or GA strand designation to calculate metrics
-    read_name = read.query_name
-    deamination_pos = [] # deaminated positions in read coordinates
-    mutation_count = 0 # Non C>T (top) or G>A (bottom) mutation count
-#    ref_S_count = 0 # total C (top) or G (bottom) reference positions
-    # TODO, delete ref_S_count if doublet approach works
-    doublets = {'AC':[0,0], 'CC': [0,0], 'GC':[0,0], 'TC':[0,0], 'OC':[0,0]} # OC represents no base/indel before C
-#    GA_to_CT_dict = {'GA': 'TC', 'GC': 'GC', 'GG': 'CC', 'GT': 'AC', 'GO': 'OC'}
-    doublet_dict = {'GA': 'TC', 'GC': 'GC', 'GG': 'CC', 'GT': 'AC', 'GO': 'OC',
-                    'AC': 'AC', 'CC': 'CC', 'GC':'GC', 'TC':'TC', 'OC':'OC'}
-
-    seq = read.query_sequence
-    seq_len = len(seq)
-    pair=read.get_aligned_pairs(matches_only=False, with_seq=True)
+    if strand in ["CT", "GA"]:
+        deamination_pos = [] # deaminated positions in read coordinates
+        doublets = {'AC':[0,0], 'CC': [0,0], 'GC':[0,0], 'TC':[0,0], 'OC':[0,0]} # OC represents no base/indel before C
+#        doublet_dict = {'GA': 'TC', 'GC': 'GC', 'GG': 'CC', 'GT': 'AC', 'GO': 'OC',
+#                    'AC': 'AC', 'CC': 'CC', 'GC':'GC', 'TC':'TC', 'OC':'OC'}
+        doublet_dict = {'GA': 'TC', 'GC': 'GC', 'GG': 'CC', 'GT': 'AC'}
         
+        if strand == "CT":
+            mutation_count = non_ref - c # 
+        else:
+            mutation_count = non_ref - g
 
-    for i, pos in enumerate(pair):
-
-
-
-        if pos[0] == None or pos[1] == None: # indel, ignore
-            continue
-        
-        strand_base = seq[pos[0]]
-        ref_base = pos[2].upper()
-
-        if ref_base != strand[0]: # ignore non C/G bases
-            if strand_base == ref_base:
-                continue
-            else:
-# Count mutations at non C (for CT) or G (for GA) positions
-                mutation_count += 1
+        for i, pos in enumerate(pair):
+            if pos[0] == None or pos[1] == None: # indel, ignore
                 continue
 
+            ref_base = pos[2].upper()
+
+            if ref_base != strand[0]: # ignore non C/G bases
+                continue
+
+            strand_base = seq[pos[0]]
+
+            if strand == 'CT':
+                doublet = pair[i-1][2].upper() + ref_base if pair[i-1][2] is not None and i>0 else 'OC'
+            elif strand == 'GA':
+#                doublet = ref_base + pair[i+1][2].upper() if i + 1 < len(pair) and pair[i+1][2] is not None else 'GO'
+                doublet = doublet_dict[ref_base + pair[i+1][2].upper()] if i + 1 < len(pair) and pair[i+1][2] is not None else 'OC'
 
 
-        if strand == 'CT':
-#            doublet = seq[pos[0]-1] + seq[pos[0]] if pos[0] > 0 else 'OC'
-            doublet = pair[i-1][2].upper() + ref_base if pair[i-1][2] is not None and i>0 else 'OC'
-        elif strand == 'GA':
-#            doublet = seq[pos[0]] + seq[pos[0]+1] if pos[0] < seq_len - 1 else 'GO'
-            doublet = ref_base + pair[i+1][2].upper() if i + 1 < len(pair) and pair[i+1][2] is not None else 'GO'
+    # Record total counts for each pair type
+#           doublets[doublet_dict[doublet]][0] += 1
+            doublets[doublet][0] += 1
 
-# Record total counts for each pair type
-        doublets[doublet_dict[doublet]][0] += 1        
 
-        if strand_base != ref_base:
-            change = ref_base + strand_base
-            if change == strand:
-# Record deamination event at each pair type
+
+            if strand_base != ref_base and strand_base == strand[1]:
                 deamination_pos.append(pos[0])
-                doublets[doublet_dict[doublet]][1] += 1
-            else:
-# Count mutations at C (for CT) or G (for GA) positions that are not deaminations
-                mutation_count += 1
+                doublets[doublet][1] += 1
 
+    else:
+        doublets = {'AC':[None,None], 'CC': [None,None], 'GC':[None,None], 'TC':[None,None], 'OC':[None,None]} # OC represents no base/indel before C
+        mutation_count = None
+        deamination_pos = None
 
-#        ref_S_count += 1
-                    
-#    deamination_rate = sum([doublets[key][1] for key in doublets])/sum([doublets[key][0] for key in doublets])
-#    print('new',deamination_rate)
+    return strand, doublets, mutation_count, deamination_pos
 
-
-#    deamination_rate_old = len(deamination_pos)/ref_S_count
-#    print('old',deamination_rate_old)
-#    mutation_rate = mutation_count/seq_len
-
-#    return deamination_rate, mutation_rate, seq_len, deamination_pos, doublets
-
-
-    return doublets, mutation_count, deamination_pos
-# output read name, strand designation, AC_count, AC_deam, CC_count, CC_deam, GC_count, GC_deam, TC_count, TC_deam, OC_count, OC_deam, mutation_count, read_len
 
 
 def strand_metrics_table (psfile, chrom, start, end, chimera_cutoff=0.9, include_readnames=None):
@@ -163,8 +142,11 @@ def strand_metrics_table (psfile, chrom, start, end, chimera_cutoff=0.9, include
             continue
         if read.is_secondary or read.is_supplementary:
             continue
+        
 
-        strand = determine_da_strand(read, chimera_cutoff)
+
+        strand, doublets, mutation_count, deam_pos = strand_metrics(read, chimera_cutoff)
+
         duplicate = read.get_tag('du') if read.has_tag('du') else "None"
 
         read_data = {           
@@ -174,33 +156,24 @@ def strand_metrics_table (psfile, chrom, start, end, chimera_cutoff=0.9, include
             'end': end, 
             'length': len(read.query_sequence),
             'strand': strand,
-            'duplicate' : duplicate
+            'duplicate' : duplicate,
+            'mutation_count' : mutation_count,
+            'deamination_positions' : ','.join(map(str, deam_pos)) if deam_pos else ''
         }
         
-
+        keys = ['AC', 'CC', 'GC', 'TC', 'OC']
 
         if strand in ['CT', 'GA']:
-            doublets, mutation_count, deam_pos = strand_metrics(read, strand)
-            read_data['mutation_count'] = mutation_count
-            read_data['deamination_positions'] = deam_pos
-            for key in ['AC', 'CC', 'GC', 'TC', 'OC']:
+            for key in keys:
                 read_data[f'{key}_count'] = doublets[key][0]
                 read_data[f'{key}_deam'] = doublets[key][1]
                 read_data[f'{key}_deam_rate'] = doublets[key][1]/doublets[key][0] if doublets[key][0] > 0 else None
-        else:
-            read_data['mutation_count'] = None
-            read_data['deamination_positions'] = None
-            for key in ['AC', 'CC', 'GC', 'TC', 'OC']:
-                read_data[f'{key}_count'] = None
-                read_data[f'{key}_deam'] = None
-                read_data[f'{key}_deam_rate'] = None
 
 
         read_collector.append(read_data)
 
     reads_table = pd.DataFrame(read_collector)
 
-    keys = ['AC', 'CC', 'GC', 'TC', 'OC']
     reads_table['total_deam'] = reads_table[[f'{key}_deam' for key in keys]].sum(axis=1)
     reads_table['total_count'] = reads_table[[f'{key}_count' for key in keys]].sum(axis=1)
     reads_table['all_deam_rate'] = (
@@ -231,9 +204,9 @@ def aggregate_strand_metrics(table):
 
         
         aggregate_template = {'chrom': group[0][0], 'start': group[0][1], 'end': group[0][2], 
-                            'strand': group[0][3], 'count':count, 'mutation_rate': mutation_rate, 'all_deam_rate': all_deam_rate,
-                            'AC_deam_rate': AC_deam_rates, 'CC_deam_rate': CC_deam_rates,
-                            'GC_deam_rate': GC_deam_rates, 'TC_deam_rate': TC_deam_rates, 'OC_deam_rate': OC_deam_rates}
+                            'strand': group[0][3], 'count':count, 'mutation_rate': ','.join(map(str, mutation_rate)), 'all_deam_rate': ','.join(map(str, all_deam_rate)),
+                            'AC_deam_rate': ','.join(map(str, AC_deam_rates)), 'CC_deam_rate': ','.join(map(str,CC_deam_rates)),
+                            'GC_deam_rate': ','.join(map(str,GC_deam_rates)), 'TC_deam_rate': ','.join(map(str,TC_deam_rates)), 'OC_deam_rate': ','.join(map(str,OC_deam_rates))}
         aggregate_collector.append(aggregate_template)
 
         aggregate_table = pd.DataFrame(aggregate_collector)
@@ -243,6 +216,8 @@ def aggregate_strand_metrics(table):
 
     return aggregate_table
 
+
+#start_time = time.time()
 
 os.makedirs(os.path.dirname(read_metrics), exist_ok=True)
 
@@ -282,16 +257,17 @@ else:
         tables.append(reg_table)
 table=pd.concat(tables, ignore_index=True)
 
+#table_time = time.time()
+#print("tables", table_time-start_time)
+
 
 aggregate_table=aggregate_strand_metrics(table)
 
 
-# save tables
-for column in ['mutation_rate', 'all_deam_rate', 'AC_deam_rate', 'CC_deam_rate', 'GC_deam_rate', 'TC_deam_rate', 'OC_deam_rate']:
-    aggregate_table[column] = aggregate_table[column].apply(lambda x: ','.join(map(str, x)) if isinstance(x, list) else '')
-    
-table['deamination_positions'] = table['deamination_positions'].apply(lambda x: ','.join(map(str, x)) if isinstance(x, list) else '')
 
 table.to_csv(read_metrics, sep='\t', index=False, compression='gzip')
 aggregate_table.to_csv(summary_metrics, sep='\t', index=False, compression='gzip')
 
+#end_time = time.time()
+#elapsed_time = end_time - start_time
+#print(f"Elapsed time: {elapsed_time:.2f} seconds")
