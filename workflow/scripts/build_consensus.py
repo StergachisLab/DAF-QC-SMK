4d210ba54657d374d299d6846a4e038181f82df7
Stephanie Bohaczuk <stephanie.bohaczuk@gmail.com>
2025-07-28 17:39:42 -0700

    Fixed bug whre the du group namesake read wasn't being captured, leading to one fewer read for building consensus.

--- workflow/scripts/build_consensus.py
+++ workflow/scripts/build_consensus.py
@@ -1,113 +1,98 @@
 import pysam
-import pandas as pd
 import pyabpoa as pa
+from collections import defaultdict
 
 
-bam_file = snakemake.input.bam
-output_bam = snakemake.output.bam
-min_read_count = snakemake.params.consensus_min_reads
-print('min_read_count:', min_read_count)
+def extract_du_seqs(pysam_file):
+    reads = defaultdict(list)
+    for read in pysam_file.fetch():
+        if read.has_tag("du"):
+            du = read.get_tag("du")
+            seq = read.query_sequence
+            reads[du].append(seq)
+        else:
+            # Reads that give the du tag name are not marked with the du tag, so this is necessary
+            du = read.query_name
+            seq = read.query_sequence
+            reads[du].append(seq)
 
-#bam_file = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/temp/htt_test/align/htt_test.filtered.bam"
-#output_bam = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/test/htt_test.consensus.bam"
-#min_read_count = 3 
-
-
-
-def bam_to_dfm(bam_file):
-    samfile = pysam.AlignmentFile(bam_file, 'rb')
-    reads = []
-    for read in samfile.fetch():
-        if read.has_tag('du'):
-            reads.append({
-                'qname': read.query_name,
-                'start': read.reference_start,
-                'end': read.reference_end,
-                'seq': read.query_sequence,
-                'du': read.get_tag('du')
-            })
-    samfile.close()
-    dfm = pd.DataFrame(reads)
-
-    return dfm
+    return reads
 
 
 def abpoa_MSA(seq_list):
-    # abPOA implementation. 
-    '''Takes list of sequences as input and returns abPOA consensus and MSA.
-    Uses abPOA built-in consensus generator'''
+    # abPOA implementation.
+    """Takes list of sequences as input and returns abPOA consensus and MSA.
+    Uses abPOA built-in consensus generator"""
     aligner = pa.msa_aligner()
     alignment = aligner.msa(seq_list, out_cons=True, out_msa=True)
 
-#    for seq in alignment.cons_seq:
-#        print(seq)
     consensus = alignment.cons_seq[0]
-
     msa_alignment = alignment.msa_seq
-
 
     return consensus, msa_alignment
 
 
-def consensus_dfm(dfm, min_read_count=3):
-    # TODO: determine what minimum read count should be
-    # TODO: subsample max read count
+def collect_consensus(dup_dict, min_read_count=3):
 
-    du_names=dfm['du'].unique()[1:]
-    dups_consensus=[]
+    du_names = dup_dict.keys()
+    dups_consensus = []
 
     for du in du_names:
-        dups_df=dfm[dfm['du']==du]
-        dup_read_count=len(dups_df)
-        if dup_read_count < min_read_count:
+        dups_read_count = len(dup_dict[du])
+        if dups_read_count < min_read_count:
             continue
-    
-        sequences=dups_df['seq'].tolist()
 
-        consensus, rep_MSA = abpoa_MSA(sequences)
-        dups_consensus.append({'du': du, 'consensus': consensus, 'read_count': dup_read_count})
+        sequences = dup_dict[du]
 
+        consensus_seq, rep_MSA = abpoa_MSA(sequences)
+        dups_consensus.append([du, consensus_seq, dups_read_count])
 
-    dups_consensus_dfm = pd.DataFrame(dups_consensus)
-
-
-    return dups_consensus_dfm
+    return dups_consensus
 
 
-def consensus_dfm_to_bam(dups_consensus_dfm, template_bam_path, output_bam_path):
-    template_bam = pysam.AlignmentFile(template_bam_path, "rb")
-    output_bam = pysam.AlignmentFile(output_bam_path, "wb", template=template_bam)
+def consensus_dfm_to_bam(dups_consensus, output_bam):
+    for du, consensus_seq, dups_read_count in dups_consensus:
+        read_name = f"{du}_consensus"
+        zm_tag = int(du.split("/")[1])
 
-
-    for index, row in dups_consensus_dfm.iterrows():
-        read_name = f"{row['du']}_consensus"
-        
         a = pysam.AlignedSegment()
         a.query_name = read_name
-        a.query_sequence = row['consensus']
+        a.query_sequence = consensus_seq
         a.flag = 4  # unmapped flag
         a.is_unmapped = True
-        a.set_tag('du', row['du'])
-        zm_tag=row['du'].split('/')[1]
-        a.set_tag('zm', int(zm_tag))
-        a.set_tag('YC', '240,187,201')
-        a.set_tag('ds', int(row['read_count']))
-
+        a.set_tag("du", du)
+        a.set_tag("zm", zm_tag)
+        a.set_tag("YC", "240,187,201") # Colors reads, useful for visualization
+        a.set_tag("dc", dups_read_count) # Duplication count
 
         output_bam.write(a)
-    
-    output_bam.close()
 
 
-def dedup_bam(bam_file, output_bam_path, min_read_count=3):
+def dedup_bam(pysam_file, output_bam, min_read_count=3):
     # First create a dfm from the input table to group reads by pbmarkdup du group tags
-    dup_dfm = bam_to_dfm(bam_file)
+    dup_dict = extract_du_seqs(pysam_file)
     # Use MSA to get consensus sequences (using default read numbers here)
-    dups_consensus_dfm = consensus_dfm(dup_dfm, min_read_count=min_read_count)
+    dups_consensus = collect_consensus(dup_dict, min_read_count=min_read_count)
     # Create an unaligned bam file from consensus sequences
-    consensus_dfm_to_bam(dups_consensus_dfm, bam_file, output_bam_path)
+    consensus_dfm_to_bam(dups_consensus, output_bam)
 
 
+# Main script execution
+bam_file = snakemake.input.bam
+output_bam = snakemake.output.bam
+min_read_count = snakemake.params.consensus_min_reads
+threads = snakemake.threads
 
+# For testing
+# bam_file = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/temp/htt_test/align/htt_test.filtered.bam"
+# output_bam = "/mmfs1/gscratch/stergachislab/bohaczuk/scripts/DAF-QC-SMK/test/htt_test.consensus.bam"
+# min_read_count = 3
+# threads = 8
 
-dedup_bam(bam_file, output_bam, min_read_count= min_read_count)
+input_bam = pysam.AlignmentFile(bam_file, "rb", threads=threads)
+output_bam = pysam.AlignmentFile(output_bam, "wb", template=input_bam, threads=threads)
+
+dedup_bam(input_bam, output_bam, min_read_count=min_read_count)
+
+input_bam.close()
+output_bam.close()
